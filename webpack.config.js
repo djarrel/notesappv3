dc35e06b2a6603878d2ecb4938db323e39e0c826
djarrel <jeffdarrellthegreat@gmail.com>
2020-02-14 15:04:50 +0300

    notesapp supplementary

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app2: ['babel-polyfill', './src/notes.app2.js'],
+        edit: ['babel-polyfill', './src/notes.edit.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        rules: [{
+            use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['env']
+            }
+            },
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: '/scripts/'
+    }
+}
+
